003ed4010dc15e2654cb1cc3ea541e39d2e98ebb
Szymon Szczerbiński <szymonszcz@student.agh.edu.pl>
2024-11-27 03:45:56 +0100

    method_3 ready

--- method_3.py
+++ method_3.py
@@ -1,22 +1,23 @@
 from docx import Document
 from docx.shared import Pt
 
+def define_vars():
+    global CSM, group1, group2, EoS
+    # Define the Character-String Mapping (CSM)
+    CSM = {
+        'A': 'WRZFOIN', 'B': 'F CWQXP', 'C': 'JMFXY Z', 'D': 'XA.MNTU',
+        'E': 'YCHIJOS', 'F': 'KUX ZJC', 'G': ' .BCPQX', 'H': 'BLOSWMI',
+        'I': 'PJGALHO', 'J': 'NSVOBGH', 'K': 'UWY.TSL', 'L': 'DIREFCQ',
+        'M': 'GHTR.WB', 'N': 'ZBADEFG', 'O': 'MNSLAB.', 'P': 'TVIZM.A',
+        'Q': 'SGKPHEM', 'R': 'HEUVIYJ', 'S': 'IYLNUAK', 'T': 'OFJGCDE',
+        'U': 'VDEHKLY', 'V': 'LONQSUR', 'W': 'QP KXRV', 'X': 'EZDYGPT',
+        'Y': '.KQUVZ ', 'Z': 'AXMTRVD', ' ': 'RTWBDNF', '.': 'CQPJ KW'
+    }
 
-# Define the Character-String Mapping (CSM)
-CSM = {
-    'A': 'WRZFOIN', 'B': 'F CWQXP', 'C': 'JMFXY Z', 'D': 'XA.MNTU',
-    'E': 'YCHIJOS', 'F': 'KUX ZJC', 'G': ' .BCPQX', 'H': 'BLOSWMI',
-    'I': 'PJGALHO', 'J': 'NSVOBGH', 'K': 'UWY.TSL', 'L': 'DIREFCQ',
-    'M': 'GHTR.WB', 'N': 'ZBADEFG', 'O': 'MNSLAB.', 'P': 'TVIZM.A',
-    'Q': 'SGKPHEM', 'R': 'HEUVIYJ', 'S': 'IYLNUAK', 'T': 'OFJGCDE',
-    'U': 'VDEHKLY', 'V': 'LONQSUR', 'W': 'QP KXRV', 'X': 'EZDYGPT',
-    'Y': '.KQUVZ ', 'Z': 'AXMTRVD', ' ': 'RTWBDNF', '.': 'CQPJ KW'
-}
-
-# Group definitions
-group1 = {' ', 'a', 's', 'n', 'r', 'u', 'm', 'f', 'b', 'c', 'v', 'z', 'x', 'q'}
-group2 = {'e', 't', 'o', 'i', 'h', 'd', 'l', 'w', 'g', 'y', '.', 'k', 'p', 'j'}
-EoS = ". . ."
+    # Group definitions
+    group1 = {' ', 'a', 's', 'n', 'r', 'u', 'm', 'f', 'b', 'c', 'v', 'z', 'x', 'q'}
+    group2 = {'e', 't', 'o', 'i', 'h', 'd', 'l', 'w', 'g', 'y', '.', 'k', 'p', 'j'}
+    EoS = ". . ."
 
 
 def gather_text(doc):
@@ -36,6 +37,7 @@ def shift_CSM(CSM, group1, char):
 
 
 def embed(path):
+    define_vars()
     cover = Document(path)
     doc = Document()
 
@@ -51,12 +53,13 @@ def embed(path):
     x = secret[idx]
     s = CSM[x.upper()]
     txt = ""
+    end = False
 
     # Embedding Procedure
     for paragraph in paragraphs:
         p = doc.add_paragraph()
         for y in paragraph:
-            if y.upper() in s:
+            if y.upper() in s and not end:
                 if txt != "":
                     r = p.add_run(txt)
                     font = r.font
@@ -82,23 +85,14 @@ def embed(path):
                     case 6:
                         font.size = Pt(def_size + 4)
 
-                #shift_CSM(CSM, group1, x.lower())
-
-                #'''
-                if x.lower() in group1:
-                    for key in CSM.keys():
-                        CSM[key] = CSM[key][1::] + CSM[key][0]  # Left Circular Shift
-                else:
-                    for key in CSM.keys():
-                        CSM[key] = CSM[key][-1] + CSM[key][:-1:]  # Right Circular Shift
-                #'''
+                shift_CSM(CSM, group1, x.lower())  # Perform Circular Shifts on CSM
 
                 if idx + 1 < len(secret):
                     idx += 1
                     x = secret[idx]
                     s = CSM[x.upper()]
                 else:
-                    continue
+                    end = True
 
             else:
                 txt += y
@@ -114,6 +108,7 @@ def embed(path):
 
 
 def extract(path, def_size):
+    define_vars()
     doc = Document(path)
 
     # Extraction Procedure
@@ -124,13 +119,13 @@ def extract(path, def_size):
         for run in paragraph.runs:
             font = run.font
             size = font.size
-            if Pt(def_size) != size:
+            if Pt(def_size) != size and not end:
                 x = run.text
                 if size == Pt(def_size - 1):
                     pos = 0
                 elif size == Pt(def_size - 2):
                     pos = 1
-                elif size == Pt(def_size -3):
+                elif size == Pt(def_size - 3):
                     pos = 2
                 elif size == Pt(def_size + 1):
                     pos = 3
@@ -148,17 +143,9 @@ def extract(path, def_size):
                     if s.index(x.upper()) == pos:
                         secret += c
                         
-                        #shift_CSM(CSM, group1, c)
+                        shift_CSM(CSM, group1, c.lower())  # Perform Circular Shifts on CSM
+                        break
 
-                        #'''
-                        if c in group1:
-                            for key in CSM.keys():
-                                CSM[key] = CSM[key][1::] + CSM[key][0]  # Left Circular Shift
-                        else:
-                            for key in CSM.keys():
-                                CSM[key] = CSM[key][-1] + CSM[key][:-1:]  # Right Circular Shift
-                            #   verify below
-                        #'''
                     if len(secret) >= 5:
                         if secret[-5::] == EoS:
                             end = True
@@ -168,9 +155,9 @@ def extract(path, def_size):
         if end:
             break
 
-    return secret
+    return secret[:-5:]
 
 
-embed("ref_m32.docx")
+embed("ref_m3.docx")
 sec = extract("Stego_method3.docx", 12)
 print(f"sec: {sec}")
